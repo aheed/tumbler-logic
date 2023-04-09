60d3c265314652190fdb9466ded7e1f817053c7c
Anders Heed <aheed88888@gmail.com>
2023-04-09 09:56:46 +0200

    export types

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+// Import types from internal files
+export { BallCollector } from './logic/BallCollector';
+export { BallDispenser } from './logic/BallDispenser';
+export { BallSink } from './logic/BallSink';
+export { getPartTypeByTool } from './logic/EditorTools';
+export { BoardModel } from './logic/model/BoardModel';
+export { PartModel } from './logic/model/PartModel';
+export { TumblerBit } from './logic/TumblerBit';
+export { TumblerBoard } from './logic/TumblerBoard';
+export { TumblerCrossover } from './logic/TumblerCrossover';
+export { TumblerEvent } from './logic/TumblerEvent';
+export { TumblerGear } from './logic/TumblerGear';
+export { TumblerGearBit } from './logic/TumblerGearBit';
+export { IGearInteractor, NullGearInteractor } from './logic/TumblerGearPart';
+export { TumblerGearPartBase } from './logic/TumblerGearPartBase';
+export { TumblerInterceptor } from './logic/TumblerInterceptor';
+export { TumblerObservable } from './logic/TumblerObservable';
+export { TumblerPart } from './logic/TumblerPart';
+export { TumblerPartFactory } from './logic/TumblerPartFactory';
+export { TumblerRamp } from './logic/TumblerRamp';
+export { TumblerBallColor, TumblerPartType, TumblerResult, IBallReceiver, ITumblerPartObserver, IReleaseButton } from './logic/TumblerTypes';
